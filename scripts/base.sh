cc6656a887f1e3eab810c7761b8ac4615304d04e
Clive Graven <cliveg@microsoft.com>
2021-02-25 13:37:42 -0700

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -84,8 +84,9 @@ EOF
 curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 #sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-#sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo yum install -y code
+ 
 # Install PowerShell
 sudo yum install -y powershell
 
