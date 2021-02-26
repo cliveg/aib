4d1d5321486be682d83f4289ba5763fb4ce97146
Clive Graven <cliveg@microsoft.com>
2021-02-25 17:55:38 -0700

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -16,7 +16,88 @@ cat >> /install/test.yml << EOF
   vars_files:
     - secrets.yml
   tasks:
-
+  - name: Install packages
+    yum:
+      name: "{{ item.pak }}"
+      state: latest
+    become_user: root
+    loop:
+      - { pak: facter }
+      - { pak: net-snmp }
+      - { pak: net-snmp-utils }
+      - { pak: crash }
+      - { pak: dstat }
+      - { pak: cifs-utils }
+      - { pak: mdadm}
+      - { pak: binutils }
+      - { pak: compat-libcap1 }
+      - { pak: compat-libstdc++-33-3.2.3-72.el7.i686 }
+      - { pak: compat-libstdc++-33-3.2.3-72.el7.x86_64 }
+      - { pak: gcc }
+      - { pak: gcc-c++ }
+      - { pak: glibc.i686 }
+      - { pak: glibc }
+      - { pak: glibc-devel.i686 }
+      - { pak: glibc-devel }
+      - { pak: ksh }
+      - { pak: libaio.i686 }
+      - { pak: libaio }
+      - { pak: libaio-devel.i686 }
+      - { pak: libaio-devel }
+      - { pak: libgcc.i686 }
+      - { pak: libgcc }
+      - { pak: libstdc++.i686 }
+      - { pak: libstdc++ }
+      - { pak: libstdc++-devel.i686 }
+      - { pak: libstdc++-devel }
+      - { pak: libXi.i686 }
+      - { pak: libXi }
+      - { pak: libXtst.i686 }
+      - { pak: libXtst }
+      - { pak: make }
+      - { pak: sysstat }
+      - { pak: java-1.8.0-openjdk }
+      - { pak: java-1.8.0-openjdk-devel }
+      - { pak: elfutils-libelf-devel }
+      - { pak: fontconfig-devel }
+      - { pak: librdmacm-devel }
+      - { pak: libstdc++-devel }
+      - { pak: nfs-utils }
+      - { pak: targetcli }
+      - { pak: libnsl.x86_64 }
+      - { pak: cloud-utils-growpart }
+      - { pak: gdisk }
+  - name: Disable SELinux
+    selinux:
+      state: disabled
+    become_user: root
+  - name: Disable Firewall daemon
+    service: 
+      name: firewalld 
+      state: stopped 
+      enabled: no
+    become_user: root
+  - name: Adjust Kernel Parameters
+    sysctl:
+      name: "{{ item.key }}"
+      value: "{{ item.value }}"
+      state: present
+    become_user: root
+    loop:
+      - { key: fs.file-max, value: 6815744 }
+      - { key: kernel.sem, value: 250 32000 100 128 }
+      - { key: kernel.shmmni, value: 4096 }
+      - { key: kernel.shmall, value: 4294967296 }
+      - { key: kernel.shmmax, value: 4398046511104 }
+      - { key: kernel.panic_on_oops, value: 1 }
+      - { key: net.core.rmem_default, value: 262144 }
+      - { key: net.core.rmem_max, value: 4194304 }
+      - { key: net.core.wmem_default, value: 262144 }
+      - { key: net.core.wmem_max, value: 1048576 }
+      - { key: net.ipv4.conf.all.rp_filter, value: 2 }
+      - { key: net.ipv4.conf.default.rp_filter, value: 2 }
+      - { key: fs.aio-max-nr, value: 1048576 }
+      - { key: net.ipv4.ip_local_port_range, value: 9000 65500 }    
   - name: Create Groups
     group:
       name: "{{ item.group }}"
@@ -78,7 +159,6 @@ cat >> /install/test.yml << EOF
   - name: Run Disable Transparent Hugepages script
     shell: ~/disable_trans_hugepages.sh
     become_user: root
-    
   - name: set up swap
     vars:
       waagent:
@@ -102,7 +182,7 @@ cat >> /install/test.yml << EOF
 
   - name: restart agent
     service:
-      name: walinuxagent
+      name: waagent.service
       state: restarted
     become_user: root
     tags:
